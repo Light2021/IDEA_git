c60e23235e751d27dfbf88de1504dc8c990a8a4d
Light2021 <1751070729@qq.com>
2020-10-10 15:48:26 +0800

    hello master

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello
     public static void main(String[] args)
     {
         System.out.println("master");
+        System.out.println("master001");
+        System.out.println("master002");
     }
 }
